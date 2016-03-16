6a1654d1446b9b1e72f4b21a00730d69ac04f53c
Dhaval Shah <dhaval@appme.sh>
2016-03-16 15:08:25 -0700

    Initial Commit

---
+++ KamcordChallenge/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KamcordChallenge
+//
+//  Created by Dhaval on 3/16/16.
+//  Copyright © 2016 Dhaval. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
